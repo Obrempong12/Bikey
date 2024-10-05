44bedbe6690fedd00377589190870d235647a3fa
Obrempong12 <tuntumbrane@gmail.com>
2024-10-05 09:30:37 -0700

    Entry Point

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables.
+dotenv.config();
+
+// Initialize express app
+const app = express();
+
+// Middleware to parse incoming JSON requests.
+app.use(express.json());
+
+// Connect to the database
+connectDB();
+
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+const rideRoutes = require('./routes/rideRoutes');
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/rides', rideRoutes);
+
+// Error handling middleware.
+const { errorHandler } = require('./utils/errorHandler');
+app.use(errorHandler);
+
+// Server listener
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
