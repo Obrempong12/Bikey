f2582a4b2d5f54ed749617a801eafb6672effb45
Obrempong12 <tuntumbrane@gmail.com>
2024-10-09 10:53:45 -0700

    UPDATED App Reactions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
+import AvailableRidesScreen from './screens/AvailableRidesScreen';
 import RideHistoryScreen from './screens/RideHistoryScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import AvailableRidesScreen from './screens/AvailableRidesScreen';
+import ProfileEditScreen from './screens/ProfileEditScreen';
 import RideTrackingScreen from './screens/RideTrackingScreen';
 import RealTimeUpdatesScreen from './screens/RealTimeUpdatesScreen';
 import InAppNotificationsScreen from './screens/InAppNotificationsScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import FeedbackScreen from './screens/FeedbackScreen';
 
 const Stack = createStackNavigator();
 
@@ -20,9 +23,12 @@ const App = () => {
         <Stack.Screen name="AvailableRides" component={AvailableRidesScreen} />
         <Stack.Screen name="RideHistory" component={RideHistoryScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="ProfileEdit" component={ProfileEditScreen} />
         <Stack.Screen name="RideTracking" component={RideTrackingScreen} />
         <Stack.Screen name="RealTimeUpdates" component={RealTimeUpdatesScreen} />
         <Stack.Screen name="InAppNotifications" component={InAppNotificationsScreen} />
+        <Stack.Screen name="Payment" component={PaymentScreen} />
+        <Stack.Screen name="Feedback" component={FeedbackScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
