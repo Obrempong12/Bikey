53a5dd00ce0aea74d5fc4675fca4efeacd0a3360
Peter Kwesi Obrempong Stephenson <144380171+Obrempong12@users.noreply.github.com>
2025-06-21 22:41:14 +0000

    Access Point

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 const userRoutes = require('./routes/userRoutes');
 const rideRoutes = require('./routes/rideRoutes');
 
-// Route
+// Routes.
 app.use('/api/users', userRoutes);
 app.use('/api/rides', rideRoutes);
 
